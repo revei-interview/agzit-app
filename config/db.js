a08a9f71c582d21286dc312d820cc093f1ebb5fd
revei-interview <revei-interview@users.noreply.github.com>
2026-03-07 23:35:18 +0530

    Fix: DB_PASS -> DB_PASSWORD env var

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port:     parseInt(process.env.DB_PORT) || 3306,
   database: process.env.DB_NAME,
   user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
